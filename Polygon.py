34dceebe0e91027ce85a0458b4a0122df9bec4e4
Rybeusz100 <jan.zych.IT@gmail.com>
2020-12-14 19:51:59 +0100

    dodana metoda print_info(), żeby było coś więcej

--- Polygon.py
+++ Polygon.py
@@ -8,3 +8,7 @@ class Polygon(ABC):
     @abstractmethod
     def is_point_inside(self, point):
         pass
+
+    def print_info(self):
+        print('Obiekt typu ' + type(self).__name__ + '\nwyznaczony przez punkty '
+              + str(self) + '\no polu równym ' + str(self.find_area()))
